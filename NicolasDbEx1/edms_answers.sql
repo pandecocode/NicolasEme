9d4110346e2ac950cb5989e22c14be2c8fbab203
Shaina Nicolas <118268735+pandecocode@users.noreply.github.com>
2024-10-19 14:09:06 +0800

    final commit

--- NicolasDbEx1/edms_answers.sql
+++ NicolasDbEx1/edms_answers.sql
@@ -1,4 +1,4 @@
--- ITEM #1
+-- ITEM 1
 /*
 create database edms;
 \c edms;
@@ -57,28 +57,28 @@ values(68319, 'KAYLING', 'PRESIDENT', NULL, '1991-11-18', 6000.00, NULL, 1001),
 	(69324, 'MARKER', 'CLERK', 67832, '1992-01-23', 1400.00, NULL, 1001);		
 */
 
--- ITEM #2
-select emp_name as "Managers who have a salary between 1500-2500"
+-- ITEM 2
+select emp_name as "Managers wih salary between 1500-2500"
 from employees
 where job_name = 'MANAGER' and salary between 1500.00 and 2500.00;
 
--- ITEM #3
+-- ITEM 3
 with employees_cte as (
 	select emp_name, job_name, (salary + coalesce(commission, 0.0)) as income 
 	from employees
 )
-select emp_name as "Salesmen who have an income above 2000"
+select emp_name as "Salesmen with an income above 2000"
 from employees_cte
 where job_name = 'SALESMAN' and income > 2000.00;
 
 -- ITEM 4
-select emp_name as "Employees that work in Sydney"
+select emp_name as "Employees working in Sydney"
 from employees emp inner join department dept
 on emp.dep_id = dept.dep_id
 where emp.dep_id = 1001;
 
 -- ITEM 5 
-select emp.emp_name as "Employees that earn more than their manager"
+select emp.emp_name as "Employees earning more than their manager"
 from employees emp inner join employees mgr 
 on emp.manager_id = mgr.emp_id
 where emp.salary > mgr.salary;
@@ -96,7 +96,7 @@ on emp.salary between sal.min_salary and sal.max_salary
 order by sal.grade;
 
 -- ITEM 8
-select distinct mgr.emp_name as "Employees who are managers of at least one salesman"
+select distinct mgr.emp_name as "Employees managing at least one salesman"
 from employees mgr join employees subordinates
 on  mgr.emp_id = subordinates.manager_id
 where subordinates.job_name = 'SALESMAN';
@@ -107,8 +107,66 @@ from department
 where dep_location = 'MELBOURNE';
 
 -- ITEM 10
+select mgr.emp_name as "Employees managing at least three people"
+from employees mgr
+where mgr.emp_id in (
+      select emp.manager_id
+      from employees emp
+      where emp.manager_id is not null 
+      and (select count(*)
+           from employees subordinates
+           where subordinates.manager_id = emp.manager_id) >= 3);
 
+-- ITEM 11
+select emp_name, rpad(to_char(salary * (1.15), 'FM$999G999D00'), 15, '-') as increased_salary
+from employees;
 
+-- ITEM 12
+select emp_name || ' & ' || job_name as "Employee & Job"
+from employees;
 
+-- ITEM 13
+--select emp_name as "Name", trim(to_char(hire_date, 'Month DD, YYYY')) as "Hired Date"
+--select emp_name || ': ' || to_char(hire_date, 'FMMonth DD, YYYY') as "Employees' Hired Date"
+select emp_name || ': ' || trim(to_char(hire_date, 'Month DD, YYYY')) as "Employees' Hired Date"
+from employees;
 
+-- ITEM 14
+select emp_name as "Employees who joined in January"
+from Employees
+where date_part('month', hire_date) = '01';
 
+-- ITEM 15
+select hire_date, job_name, emp_name
+from employees
+where hire_date < '1992-12-31';
+
+-- ITEM 16
+select emp_id, emp_name, salary
+from employees
+where hire_date < now() - interval '10 years';
+
+-- ITEM 17
+select emp_name as "Employee Names with 'AR'"
+from employees
+where emp_name like '%AR%';
+
+-- ITEM 18
+select emp.emp_name, sal.grade as salary_grade, emp.job_name,
+	case
+		when emp.salary between 2000 and 10000 then 'Supervisory'
+		when emp.salary between 1000 and 1999 then 'Team Leader'
+		when emp.salary between 700 and 999 then 'Administrative'
+		else 'NA'
+	end as salary_category
+from employees emp inner join salary_grade sal
+on emp.salary between sal.min_salary and sal.max_salary;
+
+-- ITEM 19
+select emp_name, emp_id, salary, coalesce(commission, 0.0) as commission
+from employees;
+
+-- ITEM 20
+select *
+from employees
+where commission <> 0.00;
