2051d5e174eb371aef57c46da4c550565197384f
Shaina Nicolas <118268735+pandecocode@users.noreply.github.com>
2024-10-19 12:13:30 +0800

    Update edms_answers.sql

--- NicolasDbEx1/edms_answers.sql
+++ NicolasDbEx1/edms_answers.sql
@@ -27,62 +27,86 @@ create table employees(
 	foreign key (dep_id) references department(dep_id) on delete cascade on update cascade
 );
 
-insert into employees 
-values(68319, 'KAYLING', 'PRESIDENT', NULL, '1991-11-18', 6000.00, NULL, 1001),
-		(66928, 'BLAZE', 'MANAGER', 68319, '1991-05-01', 2750.00, NULL, 3001),
-		(67832, 'CLARE', 'MANAGER', 68319, '1991-06-09', 2550.00, NULL, 1001),
-		(65646, 'JONAS', 'MANAGER', 68319, '1991-04-02', 2957.00, NULL, 2001),
-		(67858, 'SCARLET', 'ANALYST', 65646, '1997-04-19', 3100.00, NULL, 2001),
-		(69062, 'FRANK', 'ANALYST', 65646, '1991-12-03', 3100.00, NULL, 2001),
-		(63679, 'SANDRINE', 'CLERK', 69062, '1990-12-18', 900.00, NULL, 2001);
-		
-		(64989, 'ADELYN', 'SALESMAN', 66928, '1991-02-20', 1700.00, 400.00, 3001),
-		(65271, 'WADE', 'SALESMAN', 66928, '1991-02-22', 1350.00, 600.00, 3001),
-		(66564, 'MADDEN', 'SALESMAN', 66928, '1991-09-28', 1350.00, 1500.00, 3001),
-		(68454, 'TUCKER', 'SALESMAN', 66928, '1991-09-08', 1600.00, 0.00, 3001),
-		(68736, 'ADNRES', 'CLERK', 67858, '1997-05-23', 1200.00, NULL, 2001),
-		(69000, 'JULIUS', 'CLERK', 66928, '1991-12-03', 1050.00, NULL, 3001),
-		(69324, 'MARKER', 'CLERK', 67832, '1992-01-23', 1400.00, NULL, 1001); 
+insert into salary_grade
+values(1, 800, 1300),
+	(2, 1301, 1500),
+	(3, 1501, 2100),
+	(4, 2101, 3100),
+	(5, 3101, 9999);
 		
 insert into department 
 values(1001, 'FINANCE', 'SYDNEY'),
-		(2001, 'AUDIT', 'MELBOURNE'),
-		(3001, 'MARKETING', 'PERTH'),
-		(4001, 'PRODUCTION', 'BRISBANE'); 
-
-insert into salary_grade
-values(1, 800, 1300),
-		(2, 1301, 1500),
-		(3, 1501, 2100),
-		(4, 2101, 3100),
-		(5, 3101, 9999);		
+	(2001, 'AUDIT', 'MELBOURNE'),
+	(3001, 'MARKETING', 'PERTH'),
+	(4001, 'PRODUCTION', 'BRISBANE');
+		
+insert into employees
+values(68319, 'KAYLING', 'PRESIDENT', NULL, '1991-11-18', 6000.00, NULL, 1001),
+	(66928, 'BLAZE', 'MANAGER', 68319, '1991-05-01', 2750.00, NULL, 3001),
+	(67832, 'CLARE', 'MANAGER', 68319, '1991-06-09', 2550.00, NULL, 1001),
+	(65646, 'JONAS', 'MANAGER', 68319, '1991-04-02', 2957.00, NULL, 2001),
+	(67858, 'SCARLET', 'ANALYST', 65646, '1997-04-19', 3100.00, NULL, 2001),
+	(69062, 'FRANK', 'ANALYST', 65646, '1991-12-03', 3100.00, NULL, 2001),
+	(63679, 'SANDRINE', 'CLERK', 69062, '1990-12-18', 900.00, NULL, 2001),
+	(64989, 'ADELYN', 'SALESMAN', 66928, '1991-02-20', 1700.00, 400.00, 3001),
+	(65271, 'WADE', 'SALESMAN', 66928, '1991-02-22', 1350.00, 600.00, 3001),
+	(66564, 'MADDEN', 'SALESMAN', 66928, '1991-09-28', 1350.00, 1500.00, 3001),
+	(68454, 'TUCKER', 'SALESMAN', 66928, '1991-09-08', 1600.00, 0.00, 3001),
+	(68736, 'ADNRES', 'CLERK', 67858, '1997-05-23', 1200.00, NULL, 2001),
+	(69000, 'JULIUS', 'CLERK', 66928, '1991-12-03', 1050.00, NULL, 3001),
+	(69324, 'MARKER', 'CLERK', 67832, '1992-01-23', 1400.00, NULL, 1001);		
 */
 
 -- ITEM #2
---select emp_name
---from employees
---where job_name = 'MANAGER' and salary between 1500.00 and 2500.00;
+select emp_name as "Managers who have a salary between 1500-2500"
+from employees
+where job_name = 'MANAGER' and salary between 1500.00 and 2500.00;
 
 -- ITEM #3
---select emp_name
---from employees
---where (salary + commission) > 2000.00;
+with employees_cte as (
+	select emp_name, job_name, (salary + coalesce(commission, 0.0)) as income 
+	from employees
+)
+select emp_name as "Salesmen who have an income above 2000"
+from employees_cte
+where job_name = 'SALESMAN' and income > 2000.00;
 
 -- ITEM 4
---select emp_name, dep_name
---from employees emp inner join department dept
---on emp.dep_id = dept.dep_id
---where emp.dep_id = 1001;
+select emp_name as "Employees that work in Sydney"
+from employees emp inner join department dept
+on emp.dep_id = dept.dep_id
+where emp.dep_id = 1001;
 
 -- ITEM 5 
---employees that earn more than their manager
---select emp.emp_name
---from employees emp inner join employees mgr 
---on emp.manager_id = mgr.emp_id
---where emp.salary > mgr.salary
+select emp.emp_name as "Employees that earn more than their manager"
+from employees emp inner join employees mgr 
+on emp.manager_id = mgr.emp_id
+where emp.salary > mgr.salary
 
 -- ITEM 6
+select emp.emp_name as "Salesmen earning a salary grade of 2"
+from employees emp inner join salary_grade sal 
+on emp.salary between sal.min_salary and sal.max_salary
+where emp.job_name = 'SALESMAN' and sal.grade = 2;
 
+-- ITEM 7
+select emp.emp_name as "Employee Name", sal.grade as "Salary Grade"
+from employees emp inner join salary_grade sal 
+on emp.salary between sal.min_salary and sal.max_salary
+order by sal.grade;
+
+-- ITEM 8
+select distinct mgr.emp_name as "Employees who are managers of at least one salesman"
+from employees mgr join employees subordinates
+on  mgr.emp_id = subordinates.manager_id
+where subordinates.job_name = 'SALESMAN'
+
+-- ITEM 9
+select dep_name as "Departments located in Melbourne"
+from department
+where dep_location = 'MELBOURNE'
+
+-- ITEM 10
 
 
 
